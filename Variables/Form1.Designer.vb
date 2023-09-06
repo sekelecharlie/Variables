6f241facd212d55eda0007481c426b5572b1ce52
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-06 03:08:15 +0200

    Add project files.

---
+++ Variables/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        btn7 = New Button()
+        btn4 = New Button()
+        btn1 = New Button()
+        bt8 = New Button()
+        btn5 = New Button()
+        btn2 = New Button()
+        btn0 = New Button()
+        btnComma = New Button()
+        btn3 = New Button()
+        btn6 = New Button()
+        btn9 = New Button()
+        btnTimes = New Button()
+        btnMinus = New Button()
+        btnEquals = New Button()
+        btnPlus = New Button()
+        btnDivide = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(32, 26)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Greetings"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(41, 149)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(112, 34)
+        btn7.TabIndex = 1
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(41, 199)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(112, 36)
+        btn4.TabIndex = 1
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(41, 250)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(112, 36)
+        btn1.TabIndex = 1
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' bt8
+        ' 
+        bt8.Location = New Point(180, 149)
+        bt8.Name = "bt8"
+        bt8.Size = New Size(112, 36)
+        bt8.TabIndex = 1
+        bt8.Text = "8"
+        bt8.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(180, 199)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(112, 36)
+        btn5.TabIndex = 1
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(180, 250)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(112, 36)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(180, 311)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(261, 36)
+        btn0.TabIndex = 1
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnComma
+        ' 
+        btnComma.Location = New Point(41, 305)
+        btnComma.Name = "btnComma"
+        btnComma.Size = New Size(112, 36)
+        btnComma.TabIndex = 1
+        btnComma.Text = ","
+        btnComma.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(329, 250)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(112, 36)
+        btn3.TabIndex = 1
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(329, 199)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(112, 36)
+        btn6.TabIndex = 1
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(329, 147)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(112, 36)
+        btn9.TabIndex = 1
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnTimes
+        ' 
+        btnTimes.Location = New Point(465, 149)
+        btnTimes.Name = "btnTimes"
+        btnTimes.Size = New Size(112, 36)
+        btnTimes.TabIndex = 1
+        btnTimes.Text = "*"
+        btnTimes.UseVisualStyleBackColor = True
+        ' 
+        ' btnMinus
+        ' 
+        btnMinus.Location = New Point(465, 199)
+        btnMinus.Name = "btnMinus"
+        btnMinus.Size = New Size(112, 36)
+        btnMinus.TabIndex = 1
+        btnMinus.Text = "-"
+        btnMinus.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.BackColor = SystemColors.ActiveCaption
+        btnEquals.Location = New Point(99, 353)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(451, 36)
+        btnEquals.TabIndex = 1
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = False
+        ' 
+        ' btnPlus
+        ' 
+        btnPlus.Location = New Point(465, 250)
+        btnPlus.Name = "btnPlus"
+        btnPlus.Size = New Size(112, 36)
+        btnPlus.TabIndex = 1
+        btnPlus.Text = "+"
+        btnPlus.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Location = New Point(465, 305)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(112, 36)
+        btnDivide.TabIndex = 1
+        btnDivide.Text = "/"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnPlus)
+        Controls.Add(btnEquals)
+        Controls.Add(btnMinus)
+        Controls.Add(btnDivide)
+        Controls.Add(btnTimes)
+        Controls.Add(btn9)
+        Controls.Add(btn6)
+        Controls.Add(btn3)
+        Controls.Add(btnComma)
+        Controls.Add(btn0)
+        Controls.Add(btn2)
+        Controls.Add(btn5)
+        Controls.Add(bt8)
+        Controls.Add(btn1)
+        Controls.Add(btn4)
+        Controls.Add(btn7)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents bt8 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnComma As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnTimes As Button
+    Friend WithEvents btnMinus As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btnDivide As Button
+End Class
---
+++ Variables/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+        'Declaration of Variables
+        Dim strFirstName As String
+        Dim strLastName As String
+
+
+        strFirstName = "Charlie"
+        strLastName = "Sekele"
+
+        MsgBox("Hi " & strFirstName)
+        MsgBox("You are Mr " & strLastName)
+
+        MsgBox("Your full name is " & strFirstName & " " & strLastName)
+
+
+    End Sub
+End Class
